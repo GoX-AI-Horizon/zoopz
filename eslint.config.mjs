45a0a69cee9a86cda33fad5847348713873b1641
lyotchan <lyotchan66@gmail.com>
2024-12-30 16:59:23 +0000

    chore: update ESLint and Prettier configurations, adjust TypeScript target, and remove unused files

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+  ),
+  ...compat.plugins('import', 'tailwindcss', 'prettier'),
+  ...compat.config({
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+      'tailwindcss/classnames-order': 'off',
+      'prettier/prettier': [
+        'error',
+        {},
+        {
+          usePrettierrc: true,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  }),
+];
+
+export default eslintConfig;
